9891c587f04c14ee48834a36bdab38d9826b0bac
jdomagalski <78379032+jdomagalski@users.noreply.github.com>
2021-03-29 22:26:47 -0400

    Update diss_9.py

--- diss_9.py
+++ diss_9.py
@@ -6,35 +6,56 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
-    pass
+    tag = soup.find('div', class_='infocard-list infocard-list-pkmn-lg')
+    info_tag = tag.find_all('div', classe_='infocard-lg-img')[3]
+    results = info_tag.find('a')['href']
+
+
+    return 'https://pokemondb.net' + results
+
+    
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
 def getEggMoves(pokemon):
-    url = 'https://pokemondb.net/pokedex/'+pokemon
+    url = 'https://pokemondb.net/pokedex/'+ pokemon
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    moveslist = []
+    a = soup.find_all('table', classe_ = 'data-table')[2]
+    a2 = a.find_all('a', class_ = 'ent-name')
+    for move in a2:
+        moveslist.append(move.text)
+    return movestlist
+
+    #for stuff in :
+
     #add code here
 
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
 def findLetters(sentences):
     # initialize an empty list
-    
+    times_list = []
 
     # define the regular expression
-    
+    regex = '@((?[0-9]|1[0-2]) ?[ap]m'
 
     # loop through each sentence or phrase in sentences
+    for sentence in sentences:
+        x = re.findall(regex, sentence)
     
+
 
     # find all the words that match the regular expression in each sentence
        
 
     # loop through the found words and add the words to your empty list
-
+    for i in x:
+        times_list.append(i)
 
     #return the list of the last letter of all words that begin or end with a capital letter
-
-
+    return times_list
 
 def main():
     url = 'https://pokemondb.net/pokedex/national'
